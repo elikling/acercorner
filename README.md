26c8c3a5a16e6d42d63d3582fed33ce2253ac63e
eli_kling <eli_kling@hotmail.com>
2025-05-12 22:53:57 +0100

    html pages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # acercorner
+acercorner-d9fsdmgydfe9abf3.uksouth-01.azurewebsites.net
--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Captain's Log!"
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/acers')
+def acers():
+    return render_template('acers.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
